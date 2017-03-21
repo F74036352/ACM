17c4176db26e5a7da971a10b4881841386bdf2d8
F74036352 <f74036352@mail.ncku.edu.tw>
2017-03-21 12:11:13 +0800

    delete and update
    delete test file and update

--- backtracking_ DP/POJ-3624.cpp
+++ backtracking_ DP/POJ-3624.cpp
@@ -2,75 +2,9 @@
 #include <cstdlib>
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
-
-/*int main()
-{
-	typedef struct
-	{
-		int weight;
-		int value;
-		int use;
-	}ray;
-	ray array[400];
-	int n=0;
-	int tmp=0;
-	int m=0;
-	int cost=0;
-	for (int i=0;i<400;++i)
-	{
-		array[i].weight=0;
-		array[i].value=0;
-		array[i].use=0;
-	}
-	while (scanf("%d %d",&n,&m)!=EOF && n!=0)
-	{
-		tmp=0;
-		while (tmp<n)
-		{			
-			scanf("%d %d",&array[tmp].weight,&array[tmp].value);			
-			++tmp;
-		}
-		int wt=0;
-		int j=0;
-		while (m>0)
-		{
-			wt=0;
-			j=0;
-			for (int i=0;i<n;++i)
-			{
-				if (m-array[i].weight>=0 && array[i].value>=tmp && array[i].use==0)
-				{
-					wt=array[i].value;
-					j=i;
-				}
-			}
-			array[j].use=1;
-			cost+=wt;
-			m=m-array[j].weight;
-		}
-
-		printf("%d\n",cost);
-		cost=0;
-
-		for (int i=0;i<400;++i)
-		{
-			array[i].weight=0;
-			array[i].value=0;
-			array[i].use=0;
-		}
-
-
-	}
-
-
-
-
-	return 0;
-}
-*/
-//上面會run time error
 int  main()
 {
 	typedef struct
